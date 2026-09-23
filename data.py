5ed06037c98c67c069a3f8212c166feace981b39
ev <gerbspenser@gmail.com>
2026-09-23 17:24:48 +0300

    fix/Добавить константу FORM_ERROR_TITLE

--- data.py
+++ data.py
@@ -4,3 +4,4 @@ RED_COLOR_RGB = '(255, 0, 0)'
 product_name = 'Head First. Git'
 product_description = 'Книга поможет быстро и легко изучить самый популярный в мире инструмент контроля версий Git'
 product_price = 1000
+FORM_ERROR_TITLE = 'Чтобы разместить объявление, авторизуйтесь'
